c782e9ff4636eb4359dce32c3a2eccbe645c6db0
Mohammad Adyatma <mohammadadyatma@COE-M-ADYATMA.local>
2019-08-14 18:42:54 +0800

    missing )

--- Sum_test.go
+++ Sum_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestSum(t *testing.T) {
     t.Run("Main Sum", func(t *testing.T) {
 	main()
-    }
+    })
     t.Run("Normal Sum", func(t *testing.T) {
    	 total := Sum(5, 5)
    	 if total != 10 {
        	     t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
    	 }
-    }
+    })
 }
