2089a2c1d9e0698db8033ef6bcb77545b0d5a953
Mohammad Adyatma <mohammadadyatma@COE-M-ADYATMA.local>
2019-08-14 18:32:31 +0800

    Fix Sum test

--- Sum_test.go
+++ Sum_test.go
@@ -3,15 +3,16 @@ package sonarcloud_go_qscanner_travis
 import "testing"
 
 func TestSum(t *testing.T) {
-    total := Sum(5, 5)
-    if total != 10 {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+    t.Run("Main Sum", func(t *testing.T) {
+	total := main()
+	if total != 10 {
+	    t.Errorf("Summ main was incorrect, got: %d, want: %d.", total, 10)
+	}
     }
-}
-
-func TestSumMain(t *testing.T) {
-    total := main()
-    if total != 10 {
-       t.Errorf("Sum main was incorrect, got: %d, want: %d.", total, 10)
+    t.Run("Normal Sum", func(t *testing.T) {
+   	 total := Sum(5, 5)
+   	 if total != 10 {
+       	     t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+   	 }
     }
 }
